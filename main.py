2f073242e3fb9d671b28c66704aa92658c0cfebc
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-23 17:27:54 +0100

    Dane i zarys bazy

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sqlite3
 
-
+### zmienić to na nierelacyjna baze !!!! ### MONGO_DB
 class Database:
     def __init__(self):
         self.connection=sqlite3.connect("mpk.db")
         self.cursor=self.connection.cursor()
+        self.make_tables()
     def make_tables(self):
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Przystanki(IdP INTEGER NOT NULL CONSTRAINT Przystanki_pk PRIMARY KEY AUTOINCREMENT)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Przystanki(IdP INTEGER NOT NULL CONSTRAINT Przystanki_pk PRIMARY KEY AUTOINCREMENT,Nazwa TEXT NOT NULL,X REAL,Y REAL)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Linie(IdL INTEGER NOT NULL CONSTRAINT Linie_pk PRIMARY KEY AUTOINCREMENT,Symbol TEXT NOT NULL, Trasa ''')
+        self.connection.commit()
