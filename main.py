d1acd87c3cb422e36c5fa27f7130465ec6d741c0
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-24 23:37:48 +0100

    Metoda wyszukująca nazwy przystanków

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
+import json
 
-### zmienić to na nierelacyjna baze !!!! ### MONGO_DB
+# przystanki do bazy danych sql, linie i trasy do pliku json
 class Database:
     def __init__(self):
         self.connection=sqlite3.connect("mpk.db")
@@ -8,5 +9,25 @@ class Database:
         self.make_tables()
     def make_tables(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS Przystanki(IdP INTEGER NOT NULL CONSTRAINT Przystanki_pk PRIMARY KEY AUTOINCREMENT,Nazwa TEXT NOT NULL,X REAL,Y REAL)''')
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Linie(IdL INTEGER NOT NULL CONSTRAINT Linie_pk PRIMARY KEY AUTOINCREMENT,Symbol TEXT NOT NULL, Trasa ''')
         self.connection.commit()
+d=Database()
+
+s='''<a href="linie-na-przystanku-park-poludniowy-wroclaw">PARK POŁUDNIOWY</a>'''
+
+if 'wroclaw">' in s:
+    x=s[s.index(">")+1:]
+    print(x[:-4])
+stops={}
+f = open("Materials/Przystanki.txt","r",encoding="utf8")
+i=0
+l=[]
+for lines in f:
+    if 'wroclaw">' in lines:
+        i += 1
+        x = lines[lines.index(">") + 1:]
+        print(x[:-5])
+
+        dict={"id":i,"name":x[:-5]}
+        l.append(dict)
+output=open("stops.json", "w")
+json.dump(l,output,indent=4)
