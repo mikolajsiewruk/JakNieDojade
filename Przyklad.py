fd8f7368649cf7de9937b840cc4147973ab8ead2
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-29 14:09:16 +0100

    Przykład

---
+++ Przyklad.py
@@ -0,0 +1,24 @@
+
+
+class XD:
+    """
+    Class representing sample data analysis and transformation
+    """
+
+    def __init__(self,matrix:list):
+        self.matrix=matrix
+
+    def transpose(self,matrix:list)->list: # należy podać typy danych wejściowych i wyjściowych funkcji
+        """
+        Method for transposing a matrix
+        :param matrix:
+        :return: transposed matrix
+        """
+        'tu kod do transponowania itd'
+        return matrix
+
+#Podsumowując należy w potrójnym cudzyusłowie pod każdą klasą i metodą opisać co ona robi, opisać parametry, które są jej podawane i output, który returnuje
+# Robi się to po to, żeby po najechaniu kursorem na metodę mieć dostępne informacje o niej (sprawdźcie sobie)
+
+# staramy się pisać klasy nie skrypty (szersze zastosowanie kodu w przyszłości)
+# błagam tylko po angielsku piszemy !!!!!!! na pewno kod i dokumentacja po ang
