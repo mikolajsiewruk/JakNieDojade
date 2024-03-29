9d6db2f869d3d5c0f52fbe949e587778308ebf62
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-03-29 13:15:52 +0100

    Testcases

---
+++ Testcases/Test2.py
@@ -0,0 +1,8 @@
+
+# przykład z kursu Metody numeryczne i elementy optymalizacji zmienione jedynie połączenia jednostronne na dwustronne
+
+t2=[[0,2,0,5,0],
+    [2,0,3,0,7],
+    [0,3,0,1,0],
+    [5,0,1,0,4],
+    [0,7,0,4,0]]
