64bfeabe8653f3291151b9c598b758067d468072
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-05-06 20:23:41 +0200

    Revert "dodanie niedziałającego jeszcze A*"
    This reverts commit c1b7e037d07bf0912d2c7d87bbdbcd56b5595354.

--- ShortestPath.py
+++ ShortestPath.py
@@ -76,59 +76,6 @@ class ShortestPath:
         length=distances[end]["val"][1]
         return distances, path, length
 
-    def a_star(start, goal, t1):
-        open = []
-        closed = []
-        open.append(start)
-        g_of_open = []
-        h_of_open = []
-        f_of_open = []
-        g_of_open.append(0)
-        h_of_open.append(1)
-        f_of_open.append(h_of_open[0])
-        g_of_closed = []
-        neighbours = []
-        parenthood = []
-        while open:
-            index = f_of_open.index(min(f_of_open))
-            current = open[index]
-            if current == goal:
-                break
-            for i in range(len(t1)):
-                if t1[i][current] != 0:
-                    neighbours.append(i)
-            for neighbour in neighbours:
-                neighbour_cost = []
-                neighbour_cost.append(g_of_open[index] + t1[current][neighbour])
-                if neighbour in open:
-                    index_neigh_in_open = open.index(neighbour)
-                    index_neigh_in_neigh = neighbours.index(neighbour)
-                    if g_of_open[index_neigh_in_open] <= neighbour_cost[index_neigh_in_neigh]:
-                        continue
-                elif neighbour in closed:
-                    index_neigh_in_open = closed.index(neighbour)
-                    index_neigh_in_neigh = neighbours.index(neighbour)
-                    if g_of_closed[index_neigh_in_open] <= neighbour_cost[index_neigh_in_neigh]:
-                        continue
-                    closed.remove(neighbour)
-                    open.append(neighbour)
-                else:
-                    open.append(neighbour)
-                    h_of_open.append(3)  #### do heuristic to append
-                g_of_open.append(neighbour_cost)
-
-                fuckthis = True
-                for family in parenthood:
-                    if family[1] == neighbour:
-                        family[0] = current
-                        fuckthis = False
-                if fuckthis:
-                    parenthood.append([current, neighbour])
-            closed.append(current)
-        if current != goal:
-            return False
-
-
 file = open("Dane/graph.json", "r")
 graph = json.load(file)
 t=graph[0]["graph"]
