d0c41f63be5e9e68eed314841a0a6063b37076d9
Danonek97531 <d.pawliszewski@gmail.com>
2024-05-23 14:37:15 +0200

    a* commented and modified for clarity

--- a_star.py
+++ a_star.py
@@ -3,60 +3,95 @@ import numpy as np
 import json
 
 
-def f_value(node, x_goal, y_goal):
-    g_value = node[2]
-    h_value = np.sqrt((node[3]-x_goal)**2 + (node[4]-y_goal)**2)
-    f_value = g_value + h_value
-    return f_value
 
-def reconstruct_path(closed_list, current):
-    route = []
-    value = current[2]
-    while current is not None:
-        route.append(current[1])
-        parent = current[0]
-        current = next((node for node in closed_list if node[1] == parent), None)
-    return route[::-1], value
 
-def a_star(start, goal):
+def a_star(graph: list, start: int, goal: int) -> tuple:
+    """
+    A* heuristic algorithm for finding the shortest path in a graph.
+    :param graph: a graph of nodes in adjacency matrix form
+    :param start: start node
+    :param goal: goal node
+    :return: tuple
+    """
+
+    def f_value(node: list) -> float:
+        """
+        A* Helper function, determining the f_value of the given node.
+        :param node: node which f_value will be calculated
+        :return: f_value of the given node
+        """
+        g_value = node[2]
+        h_value = np.sqrt((node[3] - x_goal) ** 2 + (node[4] - y_goal) ** 2)
+        f_value = g_value + h_value
+        return f_value
+
+    def reconstruct_path(closed_list: list, current: list) -> tuple:
+        """
+        A* helper function to reconstruct the path from start to goal.
+        :param closed_list: list of all expanded nodes
+        :param current: node whose parent we search for in order to reconstruct the path
+        :return: tuple - [0] being the reconstructed path and [1] being the total cost of the path
+        """
+
+        path = []
+        value = current[2]
+        while current is not None:
+            path.append(current[1])
+            parent = current[0]
+            current = next((node for node in closed_list if node[1] == parent), None)
+        return path[::-1], value
+
     open_list = []
+    closed_list = []
     x_start = cursor.execute(f"select X from Przystanki where IdP = '{start}';").fetchone()[0]
     y_start = cursor.execute(f"select Y from Przystanki where IdP = '{start}';").fetchone()[0]
-    open_list.append([None, start, 0, x_start, y_start]) # parent, node and node's g value, x coordinate, y coordinate
-    closed_list = []
+    # THE WAY NODES IN OPEN AND CLOSED LISTS ARE STORED: [node's parent, node and node's g value, x coordinate, y coordinate]
+    open_list.append([None, start, 0, x_start, y_start])
     x_goal = cursor.execute(f"select X from Przystanki where IdP = '{goal}';").fetchone()[0]
     y_goal = cursor.execute(f"select Y from Przystanki where IdP = '{goal}';").fetchone()[0]
-    current = None
-    while open_list:
-        lowest_f = f_value(open_list[0], x_goal, y_goal)
+
+    while len(open_list) != 0:  # executing as long as there are neighbours to nodes
+
+        # determining the neighbour in open_list with the lowest f-value
+        lowest_f = f_value(open_list[0])
         current = open_list[0]
         for node in open_list:
-            if f_value(node, x_goal, y_goal) < lowest_f:
-                lowest_f = f_value(node, x_goal, y_goal)
+            if f_value(node) < lowest_f:
+                lowest_f = f_value(node)
                 current = node
+
+        # checking whether current is goal - returning the path
         if current[1] == goal:
             closed_list.append(current)
             return reconstruct_path(closed_list, current)
+
         open_list.remove(current)
         closed_list.append(current)
+        # for each node in the graph
         for i in range(len(graph[0])):
+            # if the node is not the neighbour of current or is already in closed list, skip it
             if graph[current[1]][i] == 0 or any(element[1] == i for element in closed_list):
                 continue
             else:
+                # if the neighbour is already in open list
                 if any(element[1] == i for element in open_list):
-                    neigbour_g = current[2] + graph[current[1]][i]
+                    neighbour_g = current[2] + graph[current[1]][i]
                     for j in range(len(open_list)):
                         if open_list[j][1] == i:
-                            if open_list[j][2] > neigbour_g:
+                            # If the new g value is lower than the old g value, update the neighbor’s g value and update its parent to the current node
+                            if open_list[j][2] > neighbour_g:
                                 x = cursor.execute(f"select X from Przystanki where IdP = '{j}';").fetchone()[0]
                                 y = cursor.execute(f"select Y from Przystanki where IdP = '{j}';").fetchone()[0]
-                                open_list[j] = [current[1], j, neigbour_g, x, y]
+                                open_list[j] = [current[1], j, neighbour_g, x, y]
+
+                # if the neighbour is not in the open list, add it to open list
                 else:
-                    neigbour_g = current[2] + graph[current[1]][i]
+                    neighbour_g = current[2] + graph[current[1]][i]
                     x = cursor.execute(f"select X from Przystanki where IdP = '{i}';").fetchone()[0]
                     y = cursor.execute(f"select Y from Przystanki where IdP = '{i}';").fetchone()[0]
-                    open_list.append([current[1], i, neigbour_g, x, y])
+                    open_list.append([current[1], i, neighbour_g, x, y])
 
+    # if the open list is empty and path has not been determined: no path possible, return false
     return False
 
 
@@ -64,8 +99,8 @@ connection = sqlite3.connect("mpk.db")
 cursor = connection.cursor()
 
 file = open("/Users/dominik/Documents/moje/programowanie/Phyton/Jakniedojade/JakNieDojade/Dane/graph.json", "r")
-graph = json.load(file)
+graf = json.load(file)
 
-path = a_star(1, 815)
+path = a_star(graf, 1, 815)
 print(path)
 
