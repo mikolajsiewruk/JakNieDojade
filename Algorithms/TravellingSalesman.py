b623bf7df52aa8071e31fe422ee0f0b711b47f12
Danonek97531 <d.pawliszewski@gmail.com>
2024-06-04 22:23:57 +0200

    Ukończony Held-Karp

---
+++ Algorithms/TravellingSalesman.py
@@ -0,0 +1,102 @@
+import json
+from Algorithms.ShortestPath import ShortestPath
+from Database.FindProject import find_project_root
+
+
+class TravellingSalesman:
+
+    @staticmethod
+    def held_karp(graph: list, start: int, to_visit: list) -> tuple:
+        """
+        Held Karp algorithm for solving the travelling salesman problem
+        :param graph: a graph of nodes in adjacency matrix form
+        :param start: node to start travelling salesman from
+        :param to_visit: nodes to visit in the journey
+        :return: a tuple containing the total cost of the journey, the short path and the total path to take
+        """
+
+        s = ShortestPath
+
+        def recursion(graph: list, start: int, to_visit: list, reach: int) -> list:
+            """
+           Held-Karp helper function using recursion to check each time, which node should be the second to last in our path.
+           :param graph: a new graph of nodes in adjacency matrix form
+           :param start: the node we start the journey from
+           :param to_visit: the list of nodes we need to pass in order to reach the reach
+           :param reach: ultimately the end of the journey (in the recursion: the current second to last node)
+           :return: list featuring total distance of the journey and the path to take
+           """
+            # if the length of the to_go list is equal to zero we take the value of start-reach directly from the graph
+            if len(to_visit) == 0:
+                return [graph[reach][start], [start, reach]]
+            else:
+                min_cost = float('inf')  # setting the min_cost to infinity to make the first cost be always lower than it
+                best_path = []
+                for stop in to_visit:  # removing one stop at the time from the to_go list
+                    new_to_go = to_visit.copy()
+                    new_to_go.remove(stop)  # a new list of stops to be visited (to_go) lacks one stop that becomes the reach
+                    new_to_reach = stop
+                    result = recursion(graph, start, new_to_go, new_to_reach)  # recursion checking all combinations of to_go-reach
+                    cost = result[0] + graph[new_to_reach][reach]  # the cost of going from the start to the current second to last stop
+                    path = result[1]  # the current path corresponding to the current lowest cost of the journey
+                    # updating the best cost and path after each recursion
+                    if cost < min_cost:
+                        min_cost = cost
+                        best_path = path
+                return [min_cost, best_path + [reach]]
+
+        def reconstruction(path: list, indexes: list, ultimate_paths: list) -> tuple:
+            """
+            Held-Karp helper function
+            :param path: the path from start through nodes to visit to start in a "new" form
+            :param indexes: a list  of lists containing the indexes of nodes [in new notation, in old notation]
+            :param ultimate_paths: a graph of nodes in adjacency matrix form (containing full paths between them)
+            :return: a final path in short and long form
+            """
+            final_path = []
+            final_ultimate_path = []
+
+            for i in range(len(path)):
+                start = path[i]
+                final_path.append(indexes[start][1])
+                try:
+                    stop = path[i + 1]
+                    final_ultimate_path.append(ultimate_paths[start][stop])
+                except:
+                    continue
+
+            return final_path, final_ultimate_path
+
+        stops = [start] + to_visit
+        new_graph = [[0] * len(stops) for _ in range(len(stops))]
+        indexes = []
+        ultimate_paths = [[0] * len(stops) for _ in range(len(stops))]
+        for i in range(len(stops)):
+            indexes.append([i, stops[i]])  # Each sublist contains [current_index, IdP]
+            for j in range(len(stops)):
+                result = s.dijkstra(graph, stops[i], stops[j])
+                ultimate_paths[i][j] = result[0]  # full paths between nodes in a new graph
+                new_graph[i][j] = result[1]  # graph of connections between nodes
+        new_to_visit = [index[0] for index in indexes[1:]]
+
+        recursion_results = recursion(new_graph, indexes[0][0], new_to_visit, indexes[0][0])
+        cost = recursion_results[0]
+        new_path = recursion_results[1]
+
+        reconstruction_results = reconstruction(new_path, indexes, ultimate_paths)
+        final_path = reconstruction_results[0]
+        final_ultimate_path = reconstruction_results[1]
+
+        return cost, final_path, final_ultimate_path
+
+
+# project_root = find_project_root()
+# file = open(project_root/"Dane"/"graph.json", "r")
+# graph = json.load(file)
+#
+# c = TravellingSalesman
+# results = c.held_karp(graph, 315, [127, 900, 200])
+# print(results[0])
+# print(results[1])
+# print(results[2])
+
--- held_harp.py
+++ held_harp.py
@@ -1,46 +1,53 @@
 from Algorithms.ShortestPath import ShortestPath
+import json
 
 
-t1=[[0,3,0,5,10,0,0,0,0,0],
-    [3,0,2,1,3,0,4,0,0,0],
-    [0,2,0,0,3,2,0,4,0,0],
-    [5,1,0,0,0,5,7,1,0,0],
-    [10,3,3,0,0,1,2,2,0,1],
-    [0,0,2,5,1,0,3,3,0,0],
-    [0,4,0,7,2,3,0,2,1,0],
-    [0,0,4,1,2,3,2,0,1,4],
-    [0,0,0,0,0,0,1,1,0,1],
-    [0,0,0,0,1,0,0,4,1,0]]
-
-s = ShortestPath
-
-def graph_creation(graph, start, to_visit):
+def graph_creation(graph, start, to_visit) -> tuple:
     stops = [start] + to_visit
     new_graph = [[0]*len(stops) for _ in range(len(stops))]
     indexes = []
+    paths = [[0]*len(stops) for _ in range(len(stops))]
     for i in range(len(stops)):
         indexes.append([i, stops[i]])  # Each sublist contains [current_index, IdP]
-        for j in range (len(stops)):
-            new_graph[i][j] = s.dijkstra(graph, stops[i], stops[j])[1]
-    return new_graph
+        for j in range(len(stops)):
+            result = s.dijkstra(graph, stops[i], stops[j])
+            paths[i][j] = result[0]
+            new_graph[i][j] = result[1]
+    return new_graph, indexes, paths
 
 
-def held_karp(graph: list, start: int, to_go: list, reach: int, lista = []):
+def held_karp(graph: list, start: int, to_go: list, reach: int) -> list:
+    """
+           Held Karp algorithm for solving the travelling salesman problem
+           using recursion to check each time, which node should be the second to last in our path.
+           :param graph: a graph of nodes in adjacency matrix form
+           :param start: the node we start the journey from
+           :param to_go: the list of nodes we need to pass in order to reach the reach
+           :param reach: ultimately the end of the journey (in the recursion: the current second to last node)
+           :return: list featuring total distance of the journey and the path to take
+           """
+    # if the lenght of the to_go list is equal to zero we take the value of start-reach directly from the graph
     if len(to_go) == 0:
-        return [graph[start][reach]]
+        return [graph[reach][start], [start, reach]]
     else:
-        lista.append(reach)
-        x = []
-        for stop in to_go:
+        min_cost = float('inf')  # setting the min_cost to infinity to make the first cost be always lower than it
+        best_path = []
+        for stop in to_go:  # removing one stop at the time from the to_go list
             new_to_go = to_go.copy()
-            new_to_go.remove(stop)
+            new_to_go.remove(stop)  # a new list of stops to be visited (to_go) lacks one stop that becomes the reach
             new_to_reach = stop
-            x.append(held_karp(graph, start, new_to_go, new_to_reach)[0]+graph[new_to_reach][reach])
-        return [min(x), lista]
-# no tak to wygląda aktualnie, lista się zwraca ale on przekazuje wartości do listy przy każdym przejściu rekurencji
-# przez co ta lista nie działa tak jak powinna :((
-
-
+            result = held_karp(graph, start, new_to_go, new_to_reach)  # recursion checking all combinations of to_go-reach
+            cost = result[0] + graph[new_to_reach][reach]  # the cost of going from the start to the current second to last stop
+            path = result[1]  # the current path corresponding to the current lowest cost of the journey
+            # updating the best cost and path after each recursion
+            # print("Droga ", path)
+            if cost < min_cost:
+                min_cost = cost
+                best_path = path
+            # print("Aktualne", to_go)
+            # print("Koszt", min_cost)
+            #     print("Najlepsza droga ", best_path)
+        return [min_cost, best_path + [reach]]
 
 # krótki opis jak to ma działać
 # zaczynamy w 0
@@ -50,6 +57,42 @@ def held_karp(graph: list, start: int, to_go: list, reach: int, lista = []):
 # a potem nie wiemy jaka jest min(C{},1)+graph[1][2]
 # i to już można policzyć, bo to jest graph[0][1]
 
+def reconstrucion(path, indexes, ultimate_paths):
+    final_path = []
+    big_path = []
 
-new_graph = graph_creation(t1, 2, [4,6,7])
-print(held_karp(new_graph, 0, [1,2,3], 0))
+    for i in range(len(path)):
+        start = path[i]
+        final_path.append(indexes[start][1])
+        try:
+            stop = path[i+1]
+            big_path.append(ultimate_paths[start][stop])
+        except:
+            continue
+
+    return final_path, big_path
+
+
+file1 = open("/Users/dominik/Documents/moje/programowanie/Phyton/Jakniedojade/JakNieDojade/Dane/graph.json", "r")
+t1 = json.load(file1)
+
+s = ShortestPath
+
+result = graph_creation(t1, 315, [127, 284, 315])
+new_graph = result[0]
+indexes = result[1]
+ultimate_paths = result[2]
+# print(indexes)
+print(ultimate_paths)
+
+result2 = held_karp(new_graph, 0, [1,2,3], 0)
+cost = result2[0]
+new_path = result2[1]
+print(cost)
+print(new_path)
+
+result3 = reconstrucion(new_path, indexes, ultimate_paths)
+final_path = result3[0]
+big_path = result3[1]
+print(final_path)
+print(big_path)
