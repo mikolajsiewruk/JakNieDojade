83bd4c00113c851b3b1f9b538b51cd93e0b6fe7a
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-06-08 17:39:37 +0200

    wykresy średniego czasu wykonania i średniego czasu prognozowanej drogi Plots.py

--- Visualizations/Plots.py
+++ Visualizations/Plots.py
@@ -16,6 +16,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 project = find_project_root()
+
+
 # function to execute the shortest path search with Dijkstra's, Bellman-Ford and A* algorithm.
 def execute_algorithms(args):
     start, end = args
@@ -25,45 +27,63 @@ def execute_algorithms(args):
     logging.info(f"Calculating paths from {start} to {end}")
 
     start_time = tm.perf_counter_ns()
-    sp.dijkstra(graph, start, end)
+    travel_time_dijkstra = sp.dijkstra(graph, start, end)[1]
     dijkstra_time = tm.perf_counter_ns() - start_time
 
     start_time = tm.perf_counter_ns()
-    sp.bellman_ford(graph, start, end)
+    travel_time_bf = sp.bellman_ford(graph, start, end)[1]
     bellman_ford_time = tm.perf_counter_ns() - start_time
 
     start_time = tm.perf_counter_ns()
-    sp.a_star(graph, start, end)
+    travel_time_a_star = sp.a_star(graph, start, end)[1]
     a_star_time = tm.perf_counter_ns() - start_time
 
-    return dijkstra_time, bellman_ford_time, a_star_time
+    return dijkstra_time, bellman_ford_time, a_star_time,travel_time_dijkstra,travel_time_bf,travel_time_a_star
 
 def multitask():
     with open(project /"Dane/graph.json", "r") as file:
         graph = json.load(file)
 
-    tasks = [(random.randint(1, len(graph)), random.randint(1, len(graph))) for _ in range(2)]  # generate 500 point pairs for the multiprocesses
+    tasks = [(random.randint(1, len(graph)-1), random.randint(1, len(graph)-1)) for _ in range(200)]  # generate 500 point pairs for the multiprocesses
 
-    with multiprocessing.Pool(4) as pool:
+    with multiprocessing.Pool(16) as pool:
         results = pool.map(execute_algorithms, tasks)
 
-    dijkstra_times = [result[0] for result in results]
-    bellman_ford_times = [result[1] for result in results]
-    a_star_times = [result[2] for result in results]
+    dijkstra_execution_times = [result[0] for result in results]
+    bellman_ford_execution_times = [result[1] for result in results]
+    a_star_execution_times = [result[2] for result in results]
 
-    dijkstra_mean = np.mean(dijkstra_times)
-    bf_mean = np.mean(bellman_ford_times)
-    a_star_mean = np.mean(a_star_times)
+    dijkstra_execution_mean = np.mean(dijkstra_execution_times)
+    bf_execution_mean = np.mean(bellman_ford_execution_times)
+    a_star_execution_mean = np.mean(a_star_execution_times)
+
+    dijkstra_travel_times = [result[3] for result in results]
+    bellman_ford_travel_times = [result[4] for result in results]
+    a_star_travel_times = [result[5] for result in results]
+
+    dijkstra_travel_mean = np.mean(dijkstra_travel_times)
+    bf_travel_mean = np.mean(bellman_ford_travel_times)
+    a_star_travel_mean = np.mean(a_star_travel_times)
 
     labels = ['Dijkstra', 'Bellman-Ford', 'A*']
-    avg_times = [dijkstra_mean, bf_mean, a_star_mean]
+    avg_execution_times = [dijkstra_execution_mean, bf_execution_mean, a_star_execution_mean]
 
-    plt.bar(labels, avg_times, color=['blue', 'green', 'red'])
+    plt.bar(labels, avg_execution_times, color=['blue', 'green', 'red'])
 
     plt.xlabel('Algorithm')
     plt.ylabel('Average Execution Time (ns)')
 
     plt.title('Comparison of Average Execution Times of Dijkstra and Bellman-Ford Algorithms')
+    plt.show()
+
+    labels = ['Dijkstra', 'Bellman-Ford', 'A*']
+    avg_travel_times = [dijkstra_travel_mean, bf_travel_mean, a_star_travel_mean]
+    plt.bar(labels, avg_travel_times, color=['blue', 'green', 'red'])
+
+    plt.xlabel('Algorithm')
+    plt.ylabel('Average Travel time [min]')
+
+    plt.title('Comparison of Average Travel Times of Dijkstra and Bellman-Ford Algorithms')
     plt.show()
 
 if __name__ == '__main__':
