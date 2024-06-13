430cfd1a08731a0093e22f6387006e5f7e8b5c4a
Danonek97531 <d.pawliszewski@gmail.com>
2024-06-14 00:08:49 +0200

    heuristic tsp + plots.py update

--- Algorithms/TravellingSalesman.py
+++ Algorithms/TravellingSalesman.py
@@ -45,28 +45,6 @@ class TravellingSalesman:
                         best_path = path
                 return [min_cost, best_path + [reach]]
 
-        def reconstruction(path: list, indexes: list, ultimate_paths: list) -> tuple:
-            """
-            Held-Karp helper function
-            :param path: the path from start through nodes to visit to start in a "new" form
-            :param indexes: a list  of lists containing the indexes of nodes [in new notation, in old notation]
-            :param ultimate_paths: a graph of nodes in adjacency matrix form (containing full paths between them)
-            :return: a final path in short and long form
-            """
-            final_path = []
-            final_ultimate_path = []
-
-            for i in range(len(path)):
-                start = path[i]
-                final_path.append(indexes[start][1])
-                try:
-                    stop = path[i + 1]
-                    final_ultimate_path.append(ultimate_paths[start][stop])
-                except:
-                    continue
-
-            return final_path, final_ultimate_path
-
         stops = [start] + to_visit
         new_graph = [[0] * len(stops) for _ in range(len(stops))]
         indexes = []
@@ -83,11 +61,79 @@ class TravellingSalesman:
         cost = recursion_results[0]
         new_path = recursion_results[1]
 
-        reconstruction_results = reconstruction(new_path, indexes, ultimate_paths)
+        reconstruction_results = TravellingSalesman.reconstruction(new_path, indexes, ultimate_paths)
         final_path = reconstruction_results[0]
         final_ultimate_path = reconstruction_results[1]
 
         return cost, final_path, final_ultimate_path
+
+    @staticmethod
+    def nearest_neighbors(graph: list, start: int, to_visit: list) -> tuple:
+        """
+        Nearest neighbours algorithm for solving the travelling salesman problem
+        :param graph: a graph of nodes in adjacency matrix form
+        :param start: node to start travelling salesman from
+        :param to_visit: nodes to visit in the journey
+        :return: a tuple containing the total cost of the journey, the short path and the total path to take
+        """
+
+        s = ShortestPath
+        stops = [start] + to_visit
+        new_graph = [[0] * len(stops) for _ in range(len(stops))]
+        indexes = []
+        ultimate_paths = [[0] * len(stops) for _ in range(len(stops))]
+        for i in range(len(stops)):
+            indexes.append([i, stops[i]])  # Each sublist contains [current_index, IdP]
+            for j in range(len(stops)):
+                result = s.dijkstra(graph, stops[i], stops[j])
+                ultimate_paths[i][j] = result[0]  # full paths between nodes in a new graph
+                new_graph[i][j] = result[1]  # graph of connections between nodes
+        visited = [indexes[0][0]]
+        tour = [indexes[0][0]]
+        current = indexes[0][0]
+        cost = 0
+        while len(tour) < len(new_graph):
+            min = float("inf")
+            for i in range(len(new_graph[current])):
+                if i not in visited:
+                    if new_graph[current][i] < min:
+                        min = new_graph[current][i]
+                        next = i
+            tour.append(next)
+            visited.append(next)
+            cost += new_graph[current][next]
+            current = next
+        tour.append(indexes[0][0])
+        cost += new_graph[current][indexes[0][0]]
+
+        reconstruction_results = TravellingSalesman.reconstruction(tour, indexes, ultimate_paths)
+        final_path = reconstruction_results[0]
+        final_ultimate_path = reconstruction_results[1]
+
+        return cost, final_path, final_ultimate_path
+
+    @staticmethod
+    def reconstruction(path: list, indexes: list, ultimate_paths: list) -> tuple:
+        """
+        Helper function to reconstruct the final path
+        :param path: the path from start through nodes to visit to start in a "new" form
+        :param indexes: a list  of lists containing the indexes of nodes [in new notation, in old notation]
+        :param ultimate_paths: a graph of nodes in adjacency matrix form (containing full paths between them)
+        :return: a final path in short and long form
+        """
+        final_path = []
+        final_ultimate_path = []
+
+        for i in range(len(path)):
+            start = path[i]
+            final_path.append(indexes[start][1])
+            try:
+                stop = path[i + 1]
+                final_ultimate_path.append(ultimate_paths[start][stop])
+            except:
+                continue
+
+        return final_path, final_ultimate_path
 
 
 # project_root = find_project_root()
@@ -99,4 +145,8 @@ class TravellingSalesman:
 # print(results[0])
 # print(results[1])
 # print(results[2])
-
+#
+# results2 = c.nearest_neighbors(graph, 315, [127, 900, 200])
+# print(results2[0])
+# print(results2[1])
+# print(results2[2])
